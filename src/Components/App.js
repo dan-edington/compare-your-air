73d53ea196f330d6890c0a8549423ad5f54286ff
Dan Edington <dan2505@hotmail.com>
2019-05-28 11:10:00 +0100

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+function App() {
+	return (
+		<header>
+			<h1>Compare your Air</h1>
+			<p>Compare the air quality between cities in the UK.</p>
+			<p>Select cities to compare using the search tool below.</p>
+		</header>
+	)
+}
+
+export default App
